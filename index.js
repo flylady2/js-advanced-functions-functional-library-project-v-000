b09d4bf773e1d7c1824c00e0f5133c91ee34ffdb
Leslie Stevens <flymaven@protonmail.com>
2020-01-10 15:45:22 -0600

    map and reduce

--- index.js
+++ index.js
@@ -12,12 +12,28 @@ const fi = (function() {
 
     },
 
-    map: function() {
+    map: function(collection, callback) {
+      let newCollection = []
+      const collectionToIterate = (collection instanceof Array) ? collection : Object.values(collection)
+        for (let i = 0; i < collectionToIterate.length; i++) {
+          newCollection.push(callback(collectionToIterate[i]))}
+      return newCollection
 
     },
 
-    reduce: function() {
+    reduce: function(collection, callback, startingPoint) {
+      if (!startingPoint) {
+        startingPoint = collection[0];
+        collection = collection.slice(1);
+      }
 
+      let endPoint = startingPoint
+
+      for (let i = 0; i < collection.length; i++) {
+        endPoint = callback(endPoint, collection[i])
+        console.log(endPoint)
+      }
+      return endPoint
     },
 
     functions: function() {
